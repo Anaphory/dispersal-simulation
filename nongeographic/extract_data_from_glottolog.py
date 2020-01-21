5cf02f39ea17608f6ec2ed59798505e6da954309
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2020-01-21 14:20:54 +0100

    Start working in existing data
    As in, Glottolog topologies and root ages from existing analyses

--- nongeographic/extract_data_from_glottolog.py
+++ nongeographic/extract_data_from_glottolog.py
@@ -8,10 +8,12 @@ glottolog = pyglottolog.Glottolog(cfg.get_clone("glottolog"))
 languoids = {l.id: l for l in glottolog.languoids()}
 
 
-def all_macroareas(family):
+def macroarea(family):
     macroareas = set(family.macroareas)
     for c in family.descendants_from_nodemap(languoids):
         macroareas |= set(c.macroareas)
+        if len(macroareas) > 1:
+            return []
     return macroareas
 
 families_by_macroarea = {}
@@ -23,31 +25,30 @@ for toplevel in glottolog.tree.glob("*"):
     family = glottolog.languoid(glottocode)
     if family.category == "Pseudo Family":
         continue
-    macroareas = all_macroareas(family)
-    if len(macroareas) == 1:
-        families_by_macroarea.setdefault(
-            macroareas.pop().id, []).append(family)
-    else:
-        # Some language families have a clear belonging to one macroarea, but
-        # contain off-branches or errors in other parts of the world. Clear them up.
-        macroarea = {
-            'japo1237': 'eurasia',
-            'indo1319': 'eurasia',
-            'aust1307': 'pacific',
-            'araw1281': 'southamerica',
-            'aust1305': 'eurasia',
-            'eski1264': 'northamerica',
-            'grea1241': 'eurasia',
-            'atla1278': 'africa',
-            'maya1287': 'northamerica',
-        }.get(family.id)
-        if macroarea:
-            families_by_macroarea.setdefault(
-                macroarea, []).append(family)
+    macroarea = {
+        'japo1237': 'eurasia',
+        'indo1319': 'eurasia',
+        'aust1307': 'pacific',
+        'araw1281': 'southamerica',
+        'aust1305': 'eurasia',
+        'eski1264': 'northamerica',
+        'grea1241': 'eurasia',
+        'atla1278': 'africa',
+        'maya1287': 'northamerica',
+    }.get(family.id)
+    if macroarea is None:
+        macroareas = all_macroareas(family)
+        if len(macroareas) == 1:
+            macroarea = macroareas.pop().id
         else:
+            # Ignore this language family.
             pass
+    if macroarea is not None:
+        families_by_macroarea.setdefault(macroarea, []).append(family)
 
 
+
+# Generate a Beast XML file that fits a Birth-Death tree to each continent
 root = ET.ElementTree(ET.fromstring(
     """<?xml version='1.0' encoding='UTF-8'?>
     <beast beautistatus="" beautitemplate="Standard" namespace="beast.core:beast.evolution.alignment:beast.evolution.tree.coalescent:beast.core.util:beast.evolution.nuc:beast.evolution.operators:beast.evolution.sitemodel:beast.evolution.substitutionmodel:beast.evolution.likelihood" version="2.0">
@@ -56,6 +57,7 @@ root = ET.ElementTree(ET.fromstring(
       <branchRateModel clock.rate="@clockRate.c:default" id="StrictClockModel.c:default" spec="beast.evolution.branchratemodel.StrictClockModel" />
       <run id="mcmc" spec="MCMC" chainLength="500000000" numInitializationAttempts="1000" sampleFromPrior="false">
         <state id="state" storeEvery="5000">
+            <parameter id="sampling.t" name="stateNode" value="1.0" />
             <!-- states go here -->
         </state>
         <distribution id="posterior" spec="util.CompoundDistribution">
@@ -81,23 +83,32 @@ state = root.find("./run/state")
 mcmc = root.find("./run")
 prior = root.find("./run/distribution/distribution")
 
-for macroarea, families in families_by_macroarea.items():
-    family_sizes = {
-        family.id: [
-            language.id for language in family.descendants_from_nodemap(
-                languoids, level=glottolog.languoid_levels["language"])] +
-        ([family.id] if family.level.id == 'language' else [])
-        for family in families}
+rootage = {}
 
+def family_data(family):
+    """
+
+    Parameters
+    ==========
+    family: A pyglottolog.Languoid
+    """
+    rootage.get(family.id, 7000.0)
+
+    extant_languages = [
+        language.id
+        for language in family.descendants_from_nodemap(
+                languoids, level=glottolog.languoid_levels["language"])
+        if language.endangerment.status.id != 'extinct']
+    [family.id] if family.level.id == 'language' else []
+    return family.id, age_distribution, extant_languages
+
+for macroarea, families in families_by_macroarea.items():
     ET.SubElement(
         state, "parameter",
         id="birthRate.t:{:}".format(macroarea), name="stateNode", value="8e-4")
     ET.SubElement(
         state, "parameter",
         id="deathRate.t:{:}".format(macroarea), name="stateNode", value="5e-1")
-    ET.SubElement(
-        state, "parameter",
-        id="sampling.t:{:}".format(macroarea), name="stateNode", value="2e-1")
 
     ET.SubElement(
         mcmc, "operator",
@@ -109,11 +120,6 @@ for macroarea, families in families_by_macroarea.items():
         id="birthRateScaler.t:{:}".format(macroarea),
         parameter="@birthRate.t:{:}".format(macroarea),
         scaleFactor="0.5", spec="ScaleOperator", weight="3.0")
-    ET.SubElement(
-        mcmc, "operator",
-        id="samplingScaler.t:{:}".format(macroarea),
-        parameter="@sampling.t:{:}".format(macroarea),
-        scaleFactor="0.5", spec="ScaleOperator", weight="3.0")
 
     logger = ET.SubElement(
         mcmc, "logger",
@@ -121,18 +127,21 @@ for macroarea, families in families_by_macroarea.items():
         logEvery="500")
     ET.SubElement(logger, "log", idref="birthRate.t:{:}".format(macroarea))
     ET.SubElement(logger, "log", idref="deathRate.t:{:}".format(macroarea))
-    ET.SubElement(logger, "log", idref="sampling.t:{:}".format(macroarea))
 
-    for family, languages in itertools.chain(
-            [(macroarea, ["proto-" + family for family in family_sizes])],
-            family_sizes.items()):
+    for family in itertools.chain(
+            families, [None]):
+        if family is None:
+            family, age_distribution, extant_languages = macroarea, None, families
+        else:
+            family, age_distribution, extant_languages = family_data(family)
+
         ET.SubElement(
             prior,
             "distribution",
             birthDiffRate="@birthRate.t:{:}".format(macroarea),
             id="BirthDeathModel.t:{:}".format(family),
             relativeDeathRate="@deathRate.t:{:}".format(macroarea),
-            sampleProbability="@sampling.t:{:}".format(macroarea),
+            sampleProbability="@sampling.t" if age is None else "1",
             spec="beast.evolution.speciation.BirthDeathGernhard08Model",
             tree="@Tree.t:{:}".format(family),
             type="unscaled")
@@ -179,45 +188,48 @@ for macroarea, families in families_by_macroarea.items():
             logEvery="500")
         ET.SubElement(logger, "log", idref="Tree.t:{:}".format(family))
 
-        ET.SubElement(
-            mcmc, "operator",
-            id="SubtreeSlide.t:{:}".format(family),
-            markclades="true", spec="SubtreeSlide", tree="@Tree.t:{:}".format(family),
-            weight="15.0")
-        ET.SubElement(
-            mcmc, "operator",
-            id="narrow.t:{:}".format(family),
-            markclades="true", spec="Exchange", tree="@Tree.t:{:}".format(family),
-            weight="15.0")
-        ET.SubElement(
-            mcmc, "operator",
-            id="wide.t:{:}".format(family),
-            isNarrow="false", markclades="true", spec="Exchange", tree="@Tree.t:{:}".format(family),
-            weight="3.0")
-        ET.SubElement(
-            mcmc, "operator",
-            id="WilsonBalding.t:{:}".format(family),
-            markclades="true", spec="WilsonBalding", tree="@Tree.t:{:}".format(family),
-            weight="3.0")
-        # java.lang.IllegalArgumentException: n must be positive
-        # ET.SubElement(
-        #     mcmc, "operator",
-        #     id="UniformOperator.t:{:}".format(family),
-        #     spec="Uniform", tree="@Tree.t:{:}".format(family),
-        #     weight="30.0")
+        if len(languages) > 1:
+            ET.SubElement(
+                mcmc, "operator",
+                id="SubtreeSlide.t:{:}".format(family),
+                markclades="false", spec="SubtreeSlide", tree="@Tree.t:{:}".format(family),
+                weight="15.0")
+            ET.SubElement(
+                mcmc, "operator",
+                id="narrow.t:{:}".format(family),
+                markclades="false", spec="Exchange", tree="@Tree.t:{:}".format(family),
+                weight="15.0")
+            ET.SubElement(
+                mcmc, "operator",
+                id="wide.t:{:}".format(family),
+                isNarrow="false", markclades="false", spec="Exchange",
+                tree="@Tree.t:{:}".format(family),
+                weight="3.0")
+            ET.SubElement(
+                mcmc, "operator",
+                id="WilsonBalding.t:{:}".format(family),
+                markclades="false", spec="WilsonBalding", tree="@Tree.t:{:}".format(family),
+                weight="3.0")
+            ET.SubElement(
+                mcmc, "operator",
+                id="UniformOperator.t:{:}".format(family),
+                spec="Uniform", tree="@Tree.t:{:}".format(family),
+                weight="30.0")
 
-        # Deactivate root scaling
+        if age:
+            # Deactivate root scaling
+            pass
+        else:
+            ET.SubElement(
+                mcmc, "operator",
+                id="treeScaler.t:{:}".format(family),
+                scaleFactor="0.5", spec="ScaleOperator", tree="@Tree.t:{:}".format(family),
+                weight="3.0")
+            ET.SubElement(
+                mcmc, "operator",
+                id="treeRootScaler.t:{:}".format(family),
+                rootOnly="true", scaleFactor="0.5", spec="ScaleOperator",
+                tree="@Tree.t:{:}".format(family),
+                weight="3.0")
 
-        # ET.SubElement(
-        #     mcmc, "operator",
-        #     id="treeScaler.t:{:}".format(family),
-        #     scaleFactor="0.5", spec="ScaleOperator", tree="@Tree.t:{:}".format(family),
-        #     weight="3.0")
-        # ET.SubElement(
-        #     mcmc, "operator",
-        #     id="treeRootScaler.t:{:}".format(family),
-        #     rootOnly="true", scaleFactor="0.5", spec="ScaleOperator", tree="@Tree.t:{:}".format(family),
-        #     weight="3.0")
-    break
-
-root.write("beast.xml")
+root.write("beast/birthdeathprior.xml")
