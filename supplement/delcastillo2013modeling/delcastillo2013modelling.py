cce8a541f3025cbd750580649ef46ccbedc9b1e5
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2020-02-14 17:56:42 +0100

    Reflect their basic framework

--- supplement/delcastillo2013modeling/delcastillo2013modelling.py
+++ supplement/delcastillo2013modeling/delcastillo2013modelling.py
@@ -4,3 +4,26 @@ Del Castillo and colleagues (2013) build one of the few models I am aware of tha
 
 Castillo, F. del, J. A. Barceló, L. Mameli, F. Miguel & X. Vila. 2013. Modeling Mechanisms of Cultural Diversity and Ethnicity in Hunter–Gatherers. Journal of Archaeological Method and Theory 21(2). 364–384. doi:10.1007/s10816-013-9199-y.
 """
+
+import numpy
+
+class Household:
+    maximum_steps = 1
+    tech_efficiency = 1
+
+    def __init__(self):
+        self.birth = NOW
+        cultural_identity = numpy.random.randint(size=(10, 10))
+
+    @property
+    def age(self):
+        return NOW - self.birth
+
+for time_step in range(2000):
+    survive()
+    move()
+    identify_others()
+    cooperate()
+    if not cultural_consensus():
+        work_alone()
+    die()
