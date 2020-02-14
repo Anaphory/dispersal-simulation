0df9c5f27e3f1f7bdf42281e98a679b52fcbc85e
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2020-02-14 17:40:38 +0100

    Add Del Castillo model template
    I mean, this is only the docstring, but still.

---
+++ supplement/delcastillo2013modeling/delcastillo2013modelling.py
@@ -0,0 +1,6 @@
+"""delcastillo2013modeling
+
+Del Castillo and colleagues (2013) build one of the few models I am aware of that model cultural traits as abstract numerical pattern associated to individuals, not connected to cultural practices, in a simulation of hunter-gatherer migration and population dynamics.
+
+Castillo, F. del, J. A. Barceló, L. Mameli, F. Miguel & X. Vila. 2013. Modeling Mechanisms of Cultural Diversity and Ethnicity in Hunter–Gatherers. Journal of Archaeological Method and Theory 21(2). 364–384. doi:10.1007/s10816-013-9199-y.
+"""
