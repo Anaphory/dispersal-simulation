9499d018f40badc8fa849c40af25087ddde5c3aa
Gereon Kaiping <gereon.kaiping@gmail.com>
2020-04-19 13:25:33 +0200

    Provide alternate implementation without c_int/string conversion

--- supplement/dispersal_model/hexgrid.py
+++ supplement/dispersal_model/hexgrid.py
@@ -60,3 +60,37 @@ def geographical_distance(index1: Index, index2: Index) -> meters:
     return numpy.asarray(GEODESIC.inverse(
         (lon1, lat1),
         (lon2, lat2)))[0, 0]
+
+
+# Instead of going through the h3 module, we can copy the relevant code from
+# there directly and reduce type conversions.
+Index = h3.c_int
+ring_size = int(52413 / h3.edge_length(RESOLUTION, 'm') * 1.5 + 1)
+array_len = h3.libh3.maxKringSize(ring_size)
+KringArray = h3.H3Index * array_len
+DistanceArray = h3.c_int * array_len
+krings = KringArray()
+distances = DistanceArray()
+
+
+def neighbors_within_distance(address: Index, _=None) -> Iterator[Index]:
+    """Get K-Rings for a given hexagon properly split by ring"""
+    # Initializes to zeroes by default, don't need to force
+    h3.libh3.kRingDistances(address, ring_size, krings, distances)
+    for i in range(0, array_len):
+        if krings[i] != 0:
+            yield krings[i]
+
+
+def geo_coordinates(address: Index) -> Tuple[float, float]:
+    geo_coord = h3.GeoCoord()
+    h3.libh3.h3ToGeo(address, h3.byref(geo_coord))
+    return (
+        h3.mercator_lng(h3.rads_to_degs(geo_coord.lng)),
+        h3.mercator_lat(h3.rads_to_degs(geo_coord.lat)))
+
+
+def closest_grid_point(longitude: float, latitude: float) -> Index:
+    geo_coord = h3.GeoCoord(
+        h3.degs_to_rads(latitude), h3.degs_to_rads(longitude))
+    return h3.libh3.geoToH3(h3.byref(geo_coord), RESOLUTION)
