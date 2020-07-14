1d2280f4bd5ef1ebd59d4af1818a4a5d46f0af13
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2020-07-14 17:24:44 +0200

    Adapt debug output to changing API

--- supplement/dispersal_model_rust/src/debug.rs
+++ supplement/dispersal_model_rust/src/debug.rs
@@ -1,23 +1,20 @@
 impl std::fmt::Debug for crate::Patch {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Patch")
-            .field("r", &self.resources)
-            .field("/", &self.max_resources)
+            .field("r", &self.resources.iter().sum::<f32>())
+            .field("/", &self.max_resources.iter().sum::<f32>())
             .finish()
     }
 }
 
 impl std::fmt::Debug for crate::Family {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let geo = crate::hexgrid::geo_coordinates(self.location);
 
         f.debug_struct("Family")
             .field("descendence", &self.descendence)
             .field("size", &self.effective_size)
             .field("seasons_till_next_child", &self.seasons_till_next_child)
             .field("location", &self.location)
-            .field("lat", &(geo.lat * 180. / crate::PI))
-            .field("lon", &(geo.lon * 180. / crate::PI))
             .field("culture", &format_args!("{:020b}", &self.culture))
             .field("stored_resources", &self.stored_resources)
             .finish()
