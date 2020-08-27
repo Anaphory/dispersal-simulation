767d89cb5ebd700af1f1aad10ebdd874d47ce844
Gereon Kaiping <gereon.kaiping@gmail.com>
2020-08-27 04:03:59 +0200

    This code works, but it is slow.

--- supplement/distances/reduce.py
+++ supplement/distances/reduce.py
@@ -36,6 +36,7 @@ from h3.h3 import H3Index as Index
 
 from database import db
 from ecoregions import ECOREGIONS, TC, RESOLUTION
+from distances import lonlat
 
 if __name__ == '__main__':
     import argparse
@@ -65,8 +66,8 @@ if __name__ == '__main__':
         print(i)
         query = sqlalchemy.select([
             t_dist.c.hexbin1, t_dist1.c.hexbin2,
-            func.min(t_dist.c.distance + t_dist1.c.distance),
-            20]
+            t_dist.c.distance + t_dist1.c.distance,
+            t_dist.c.source * t_dist1.c.source]
         ).select_from(
             t_dist.join(
                 t_dist1,
@@ -80,18 +81,18 @@ if __name__ == '__main__':
                 isouter=True
             )
         ).where(
-            (t_dist2.c.hexbin1 == None) &
-            (t_dist2.c.hexbin2 == None) &
+            ((t_dist2.c.hexbin1 == None) &
+             (t_dist2.c.hexbin2 == None)) |
+            (t_dist2.c.distance > t_dist.c.distance + t_dist1.c.distance) &
             (t_dist.c.distance + t_dist1.c.distance < 3*8*60*60)
-        ).group_by(
-            t_dist.c.hexbin1, t_dist1.c.hexbin2
         )
         shortcuts = t_dist.insert().from_select(
             [t_dist.c.hexbin1, t_dist.c.hexbin2,
              t_dist.c.distance, t_dist.c.source],
-            query)
-        engine.execute(shortcuts)
-        engine.execute(t_eco.delete(whereclause=t_eco.c.hexbin == i))
-        engine.execute(t_dist.delete(whereclause=t_dist.c.hexbin1 == i))
-        engine.execute(t_dist.delete(whereclause=t_dist.c.hexbin2 == i))
-        engine.execute(t_hex.delete(whereclause=t_hex.c.hexbin == i))
+            query).prefix_with("OR REPLACE")
+        with engine.begin() as conn:
+            conn.execute(shortcuts)
+            conn.execute(t_eco.delete(whereclause=t_eco.c.hexbin == i))
+            conn.execute(t_dist.delete(whereclause=t_dist.c.hexbin1 == i))
+            conn.execute(t_dist.delete(whereclause=t_dist.c.hexbin2 == i))
+            conn.execute(t_hex.delete(whereclause=t_hex.c.hexbin == i))
